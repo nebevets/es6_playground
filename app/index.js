fb01b4893dadc07363dbea055e948ae6cb1aa3c4
nebevets <steveben@exmsft.com>
2018-09-12 14:49:44 -0700

    added Person and SuperPerson classes

--- app/index.js
+++ app/index.js
@@ -1,6 +1,13 @@
-import addToDom from './add_to_dom';
-//import {sub, mult} from './math';
-import * as math from './math';
+// import addToDom from './add_to_dom';
+// //import {sub, mult} from './math';
+// import * as math from './math';
 
-addToDom('h1', `52 - 10 = ${math.sub(52,10)}`);
-addToDom('h2', `52 - 10 = ${math.mult(52,10)}`);
+// addToDom('h1', `52 - 10 = ${math.sub(52,10)}`);
+// addToDom('h2', `52 - 10 = ${math.mult(52,10)}`);
+
+import SuperPerson from './person';
+
+const bob = new SuperPerson('Bob', 'invisibility');
+bob.greet();
+
+bob.saySuperPower();
---
+++ app/person.js
@@ -0,0 +1,27 @@
+import addToDom from './add_to_dom';
+
+class Person {
+    constructor(name){
+        this.name = name;
+    }
+
+    talk(message){
+        addToDom('h1', `${this.name} says: ${message}`);
+    }
+    greet(){
+        this.talk(`Hi, my name is ${this.name}`);
+    }
+}
+
+class SuperPerson extends Person{
+    constructor(name, superPower){
+        super(name);
+        this.superPower = superPower;
+    }
+
+    saySuperPower(){
+        this.talk(`My super power is ${this.superPower}`);
+    }
+}
+
+export default SuperPerson;
